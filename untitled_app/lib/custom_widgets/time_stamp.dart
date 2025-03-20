2f11d469e1d7328be7261f77854b5cbbb1ad4641
Eric Chuang <ericchuang94@gmail.com>
2025-03-19 23:46:02 -0700

    change timestamp

--- untitled_app/lib/custom_widgets/time_stamp.dart
+++ untitled_app/lib/custom_widgets/time_stamp.dart
@@ -1,46 +1,50 @@
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 
+String formatTime(String time) {
+  DateTime now = DateTime.now();
+  DateTime postTime = DateTime.parse(time).toLocal();
+  Duration difference = now.difference(postTime);
 
-  String formatTime(String time) {
-    DateTime now = DateTime.now();
-    DateTime postTime = DateTime.parse(time).toLocal();
-    Duration difference = now.difference(postTime);
+  int totalMonths = 0;
+  totalMonths = (now.year - postTime.year) * 12;
+  totalMonths += now.month - postTime.month;
 
-    if (difference.inDays < 1) {
-      if (difference.inHours < 1) {
-        if (difference.inMinutes < 1) {
-          return '1m';
-        } else {
-          return '${difference.inMinutes}m';
-        }
-      } else {
-        return '${difference.inHours}h';
-      }
-    } else if (difference.inDays > 3) {
-      return DateFormat('M/d').format(postTime);
-    } else {
-      return '${difference.inDays}d';
-    }
+  if (now.day < postTime.day) {
+    totalMonths--;
   }
-  
+  int years = totalMonths ~/ 12;
+  int days = difference.inDays;
+
+  if (totalMonths >= 12) {
+    return '${years}y';
+  } else if (totalMonths > 0) {
+    return '${totalMonths}mo';
+  } else if (days > 0) {
+    return '${days}d';
+  } else if (difference.inHours > 0) {
+    return '${difference.inHours}h';
+  } else if (difference.inMinutes > 0) {
+    return '${difference.inMinutes}m';
+  } else {
+    return 'now';
+  }
+}
+
 class TimeStamp extends StatelessWidget {
   final String time;
   final double fontSize;
   const TimeStamp({super.key, required this.time, this.fontSize = 15});
-
-
 
   @override
   Widget build(BuildContext context) {
     return Text(
       formatTime(time),
       style: TextStyle(
-        fontSize: fontSize,
-        //fontWeight: FontWeight.w300,
-        color: Theme.of(context).colorScheme.onSurfaceVariant
-        //color: Theme.of(context).colorScheme.onBackground,
-      ),
+          fontSize: fontSize,
+          //fontWeight: FontWeight.w300,
+          color: Theme.of(context).colorScheme.onSurfaceVariant
+          //color: Theme.of(context).colorScheme.onBackground,
+          ),
     );
   }
 }
